99ca68f35ec3e08d0d5cdef5856ed4bf15991dcd
Achyuth A <achyuth248@gmail.com>
2025-04-16 15:30:57 +0530

    hero progress

---
+++ app/components/Hero.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+    return (
+      <div className="hero">
+        <div className="hero-text">
+          <h2 className="text-content">EMPOWER</h2>
+          <h2 className="text-content">EMPOWER</h2>
+          <h2 className="text-content">EMPOWER</h2>
+          <h2 className="text-content">EMPOWER</h2>
+          <div className="line-combo">
+            <h2 className="text-content">EMPOWER</h2>
+            <p className="with-text">with</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
 
       <main>
-          <h2></h2>
+        <Hero />
       </main>
   );
 }
